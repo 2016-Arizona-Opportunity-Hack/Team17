ee9fa455b053240779e787c26f8cb3a6dab30ccd
Lucky-PC\Lucky <klakshmisagar07@gmail.com>
2016-10-01 22:31:55 -0700

    Created and implemented the functionality of Manual

---
+++ app/src/main/java/com/readonchandler/view/Manuals.java
@@ -0,0 +1,20 @@
+package com.readonchandler.view;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.readonchandler.R;
+
+/**
+ * Created by Lakshmisagar on 10/1/2016.
+ */
+
+public class Manuals extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.layout_manual);
+    }
+}
