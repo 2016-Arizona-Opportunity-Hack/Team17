faccce772c96efdfc1944cc55d3b6f9fd5baf5f6
Lucky-PC\Lucky <klakshmisagar07@gmail.com>
2016-10-01 19:28:48 -0700

    Cretaed Calendar list events for perticular day

--- app/src/main/java/com/readonchandler/view/CustomCalendarAdapter.java
+++ app/src/main/java/com/readonchandler/view/CustomCalendarAdapter.java
@@ -1,6 +1,7 @@
 package com.readonchandler.view;
 
 import android.content.Context;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +17,7 @@ import java.util.List;
  */
 
 public class CustomCalendarAdapter extends BaseAdapter {
+    private static final String TAG = CustomCalendarAdapter.class.getName();
     Context mContext;
     LayoutInflater inflater;
     private List<EventsModel> events_list;
@@ -43,16 +45,18 @@ public class CustomCalendarAdapter extends BaseAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        Log.d(TAG,"getView");
         if (inflater == null) {
             inflater = (LayoutInflater) mContext.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         }
         if (convertView == null)
             convertView = inflater.inflate(R.layout.calendar_day_list_row, null);
 
-        TextView title = (TextView) convertView.findViewById(R.id.eventTime);
+        TextView title = (TextView) convertView.findViewById(R.id.eventTitle);
         TextView time = (TextView) convertView.findViewById(R.id.eventTime);
 
         // getting eventlist data for the row
+
         EventsModel m = events_list.get(position);
         title.setText(m.getTitle());
         time.setText(m.getTime());
