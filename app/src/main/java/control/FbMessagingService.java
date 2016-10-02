23f94d8771aed5a074c73e069dac5ca9e147c521
Lucky-PC\Lucky <klakshmisagar07@gmail.com>
2016-10-02 07:35:38 -0700

    Written Parsing logic

--- app/src/main/java/control/FbMessagingService.java
+++ app/src/main/java/control/FbMessagingService.java
@@ -1,15 +1,23 @@
 package control;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.widget.Toast;
 
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
+
+import database.DBContentProvider;
+import model.Event;
+import model.Manual;
 
 /**
  * Created by "nithesh" on 10/2/2016.
  */
 
 public class FbMessagingService extends FirebaseMessagingService {
+    Event event;
+    Manual manual;
+    SQLiteDatabase db;
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
         //super.onMessageReceived(remoteMessage);
@@ -19,8 +27,28 @@ public class FbMessagingService extends FirebaseMessagingService {
         message = message+"From: " + remoteMessage.getFrom();
 
         // Check if message contains a data payload.
+
+        //Added the parsing logic
         if (remoteMessage.getData().size() > 0) {
             message = message+"Message data payload: " + remoteMessage.getData();
+            String string = remoteMessage.getData().toString();
+            String [] result = string.split(",");
+            int i = 0;
+            if(result[i++].equals("Event")) {
+                event.setName(result[i++]);
+                event.setDate(result[i++]);
+                event.setTime(result[i++]);
+                event.setLatitude(Double.parseDouble(result[i++]));
+                event.setLongitude(Double.parseDouble(result[i++]));
+                event.setVideoLink(result[i++]);
+                event.setEventDetails(result[i++]);
+
+                DBContentProvider.insertEvent(db, event);
+            }else{
+                manual.setName(result[i++]);
+                manual.setLink(result[i++]);
+                DBContentProvider.insertManual(db,manual);
+            }
         }
 
         // Check if message contains a notification payload.
